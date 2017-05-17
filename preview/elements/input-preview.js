def44ba9b481b5c411ba16b03ae2eaafb7253a1b
Johnny Wu <KarasAya@gmail.com>
2017-05-17 11:52:10 +0800

    add input

---
+++ preview/elements/input-preview.js
@@ -0,0 +1,32 @@
+module.exports = function (viewEL) {
+  let eventEL = viewEL.querySelector('#event');
+
+  // g-01
+  ['.g-01', '.g-02'].forEach(g => {
+    let target = viewEL.querySelector(`${g} ui-input`);
+
+    let text = viewEL.querySelector(`${g} span.text`);
+    text.innerHTML = target.value;
+
+    target.addEventListener('change', event => {
+      let text = viewEL.querySelector(`${g} span.text`);
+      text.innerHTML = event.detail.value;
+
+      viewEL.updateEventText(eventEL, 'change');
+    });
+
+    target.addEventListener('confirm', () => {
+      viewEL.updateEventText(eventEL, 'confirm');
+    });
+
+    target.addEventListener('cancel', () => {
+      viewEL.updateEventText(eventEL, 'cancel');
+    });
+  });
+
+  let target = viewEL.querySelector(`.g-03 [disabled]`);
+  let btn = viewEL.querySelector(`#focus`);
+  btn.addEventListener('click', () => {
+    target.focus();
+  });
+};
