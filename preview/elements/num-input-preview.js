2b1c5d3b750076e5a9a75e45d4fa353d3c65f67e
VisualSJ <devhacker520@gmail.com>
2018-01-25 09:41:25 +0800

    The button is arranged in percentage

--- preview/elements/num-input-preview.js
+++ preview/elements/num-input-preview.js
@@ -1,34 +1,34 @@
 'use strict';
 
 module.exports = function (viewEL) {
-    let eventEL = viewEL.querySelector('#event');
+  let eventEL = viewEL.querySelector('#event');
 
-    // g-01
-    ['.g-01', '.g-02'].forEach(g => {
-        let target = viewEL.querySelector(`${g} ui-num-input`);
+  // g-01
+  ['.g-01', '.g-02'].forEach(g => {
+    let target = viewEL.querySelector(`${g} ui-num-input`);
 
-        let text = viewEL.querySelector(`${g} span.text`);
-        text.innerHTML = target.value;
+    let text = viewEL.querySelector(`${g} span.text`);
+    text.innerHTML = target.value;
 
-        target.addEventListener('change', event => {
-            let text = viewEL.querySelector(`${g} span.text`);
-            text.innerHTML = event.detail.value;
+    target.addEventListener('change', event => {
+      let text = viewEL.querySelector(`${g} span.text`);
+      text.innerHTML = event.detail.value;
 
-            viewEL.updateEventText(eventEL, 'change');
-        });
-
-        target.addEventListener('confirm', () => {
-            viewEL.updateEventText(eventEL, 'confirm');
-        });
-
-        target.addEventListener('cancel', () => {
-            viewEL.updateEventText(eventEL, 'cancel');
-        });
+      viewEL.updateEventText(eventEL, 'change');
     });
 
-    let target = viewEL.querySelector(`.g-03 [disabled]`);
-    let btn = viewEL.querySelector(`#focus`);
-    btn.addEventListener('click', () => {
-        target.focus();
+    target.addEventListener('confirm', () => {
+      viewEL.updateEventText(eventEL, 'confirm');
     });
+
+    target.addEventListener('cancel', () => {
+      viewEL.updateEventText(eventEL, 'cancel');
+    });
+  });
+
+  let target = viewEL.querySelector(`.g-03 [disabled]`);
+  let btn = viewEL.querySelector(`#focus`);
+  btn.addEventListener('click', () => {
+    target.focus();
+  });
 };
